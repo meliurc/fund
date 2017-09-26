7e474c0b92243cdc81aadc2675d42e2b9aa00b92
meliurc <lrc80>
2017-09-26 22:26:21 +0800

    fix comp_104_fund.py

--- fund_spider/comp_104_fund.py
+++ fund_spider/comp_104_fund.py
@@ -4,15 +4,23 @@ from fund_spider.spiders.fund_spider import FundSpider
 
 
 class CompSpider(FundSpider):
-    pass
+    con_log = ''
 
-f = open('scrapy.cfg', 'r')
+    def closed(self, reason):
+        pass
+
+f = open('connection_104_log.txt', 'r')
 code_list = f.read().splitlines()
+print code_list
 
+f2 = open('fix_connection_104_error_log.txt', 'w')
 spider = CompSpider()
 while code_list != []:
     code = code_list[0]
     status = spider.to_price_list(code)
-    print status
     if status != 'ConnectionError 104':
         code_list.remove(code)
+        f2.write(code + '\t' + status + '\n')
+
+f.close()
+f2.close()
